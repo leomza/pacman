32de0f6e484e8200e6a57dfc42dc43f68efe5b57
leomza <leo_mza1991@hotmail.com>
2021-08-16 18:46:04 +0300

    Change in comments

--- script.js
+++ script.js
@@ -128,5 +128,3 @@ function handleSubmit(ev) {
         console.error(error)
     }
 }
-
-//I know that I didnt apply "import and export" between listeners.js and script.js, however I told that to Tal and he is going to teach that soon (I tried to do it by myself but I couldn`t
